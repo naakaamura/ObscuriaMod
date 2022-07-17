cf69a6474f2d207439e06c3310b4dfbcd87a57f2
naka <joaovtnakamura@gmail.com>
2022-07-17 01:52:58 -0300

    Item drops

---
+++ Content/Items/Placeables/ObscureStoneItem.cs
@@ -0,0 +1,21 @@
+﻿using ObscuriaMod.Content.Tiles.ObscureBadlands;
+using Terraria.ModLoader;
+
+namespace ObscuriaMod.Content.Items.Placeables
+{
+    public sealed class ObscureStoneItem : ModItem
+    {
+        public override void SetStaticDefaults() {
+            DisplayName.SetDefault("Obscure Stone");
+        }
+
+        public override void SetDefaults() {
+            Item.maxStack = 999;
+
+            Item.width = 16;
+            Item.height = 16;
+
+            Item.DefaultToPlaceableTile(ModContent.TileType<ObscureStoneTile>());
+        }
+    }
+}
