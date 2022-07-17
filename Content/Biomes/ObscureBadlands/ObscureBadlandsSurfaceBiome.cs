0a18e8fce94a3b98640e682a2e0be5fcecc6dc23
naka <joaovtnakamura@gmail.com>
2022-07-17 02:02:03 -0300

    Biome exists now

---
+++ Content/Biomes/ObscureBadlands/ObscureBadlandsSurfaceBiome.cs
@@ -0,0 +1,21 @@
+﻿using ObscuriaMod.Common.Systems;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace ObscuriaMod.Content.Biomes.ObscureBadlands
+{
+    public sealed class ObscureBadlandsSurfaceBiome : ModBiome
+    {
+        public override int Music => MusicLoader.GetMusicSlot("ObscuriaMod/Assets/Sounds/Music/MainTheme");
+
+        public override void SetStaticDefaults() {
+            DisplayName.SetDefault("Obscure Badlands");
+        }
+
+        public override bool IsBiomeActive(Player player) {
+            bool onSurface = player.ZoneSkyHeight || player.ZoneOverworldHeight;
+            bool onBiome = ObscuriaTileCountSystem.ObscureBadlandsTileCount > 100;
+            return onBiome && onSurface;
+        }
+    }
+}
